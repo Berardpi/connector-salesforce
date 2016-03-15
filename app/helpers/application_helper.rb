241c30e710d2edc39b441c7fcd98f43603da0287
Pierre Berard <pierre.berard@maestrano.com>
2016-03-15 15:20:05 +0000

    Adds custom css for nab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def current_tenant
+    return default_tenant unless session[:tenant]
+
+    case session[:tenant]
+    when 'nab'
+      {nid: 'nab', name: 'NAB', copyright: '© NAB something'}
+    else
+      default_tenant
+    end
+  end
+
+  def default_tenant
+    {nid: 'maestrano', name: 'Maestrano', copyright: '© Copyright 2016 Maestrano Pty Ltd (ABN: 80 152 564 424). All rights reserved. Various trademarks held by their respective owners.'}
+  end
 end
